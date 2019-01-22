98cd7270a0cd6630ceaa91fc13054cb22c5effcf
Marek <mrkkr91@gmail.com>
2019-01-22 22:57:21 +0100

    Change some comments in code

--- email_scraper.py
+++ email_scraper.py
@@ -8,25 +8,25 @@ import re
 import csv
 import fileinput
 
-# a queue of urls to be crawled, lista zaimportowanych URLi z CSV
+# a queue of urls to be crawled
 new_urls = deque()
 
-# URLs z CSV
+# a queue URLs from CSV
 csv_urls = deque()
 
-# otwiera plik CSV, linki z każdego wiersza są dodawane do csv_urls, plik musi być w tym samym folderze
+# open CSV file, URL from each separate line are added to csv_urls
 with open('', newline='') as r:
     reader = csv.reader(r)
     for row in reader:
         csv_urls.extend(row)
 
-# pętla URLi z CSV
+# loop from links
 while len(csv_urls):
     print('csv_urls')
     print(csv_urls)
     url_csv = csv_urls.popleft()
 
-    #dodaje pojedynczy URL z CSV do new_urls, który będzie przetwarzany
+    # add separated URL to be crawled from CSV to new_urls pojedynczy URL z CSV do new_urls
     new_urls.append(url_csv)
 
     # a set of urls that we have already crawled
@@ -35,12 +35,12 @@ while len(csv_urls):
     # a set of crawled emails
     emails = set()
 
-    # process urls one by one until we exhaust the queue
+    # process URLs one by one until we exhaust the queue
     while len(new_urls):
         print('new_urls')
         print(new_urls)
 
-        # move next url from the queue to the set of processed urls
+        # move next URL from the queue to the set of processed URLs
         url = new_urls.popleft()
         processed_urls.add(url)
 
@@ -56,7 +56,7 @@ while len(csv_urls):
         except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout,
                 requests.exceptions.TooManyRedirects, requests.exceptions.InvalidURL, requests.exceptions.ChunkedEncodingError,
                 requests.exceptions.ContentDecodingError, requests.exceptions.HTTPError):
-            # ignore pages with errors # dodałem 4 nowe wyjątki błędów/ostrzeżeń 
+            # ignore pages with errors above
             continue
 
         #save email result to CSV
@@ -72,36 +72,31 @@ while len(csv_urls):
             for item in new_emails:
                 spamwriter.writerows([item.split(' ')])
 
-        # create a beutiful soup for the html document
+        # create a beautiful soup for the html document
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # find and process all the anchors in the document
         for anchor in soup.find_all("a"):
             # extract link url from the anchor
             link = anchor.attrs["href"] if "href" in anchor.attrs else ''
-            # resolve relative links
+            # resolve relative link
             if link.startswith('/'):
                 link = base_url + link
             elif not link.startswith('http'):
                 link = path + link
-            # pomijaj link jeśli NIE zaczyna się na path (czyli jeśli nie jest w obrębie tej samej domeny)
+            # skip link if is not come from the same domain
             elif link.startswith(path) == False:
                 continue
-            # pomijaj link jeśli kończy się rozszerzeniami zdjęć, grafik, styli CSS, skryptów JS, PDF, itd...
-            if link.endswith( ('.jpg', '.JPG', '.jpeg', '.ico', '.png', '.gif', '.eps', '.pdf', '.css', '.csv', '.js', '.zip', '.rar', 
-                '.mp4', '.mp3', '.ogg', '.mov', '.m4v', '.webm', '.ogv', '.swf', 'void(0);', '.xls', '.xlsx', '.doc', '.docx', '#', '#0',
-                'cart') ) == True:
+            # skip link with extensions below
+            if link.endswith( ('.jpg', '.JPG', '.jpeg', '.ico', '.png', '.gif', '.eps', '.pdf', '.css', '.csv', '.js', '.zip', '.rar', '.mp4', '.mp3', '.ogg', '.mov', '.m4v', '.webm', '.ogv', '.swf', 'void(0);', '.xls', '.xlsx', '.doc', '.docx', '#', '#0', 'cart') ) == True:
                 continue
-            # pomijaj link, który posiada w adresie jedno z następujących słów:
-            prefixy_1 = ('javascript:', 'blog','Blog', 'category', 'calendar', 'facebook', 'twitter', 'linkedin', 'instagram', 'tel:+', 'tel:',
-                'sitemap', 'site-map', 'mailto', 'privacy-policy', 'products', 'news')
+            # skip link if HAS one of the following words:
+            prefixy_1 = ('javascript:', 'blog','Blog', 'category', 'calendar', 'facebook', 'twitter', 'linkedin', 'instagram', 'tel:+', 'tel:', 'sitemap', 'site-map', 'mailto', 'privacy-policy', 'products', 'news')
             if any(s in link for s in prefixy_1):
                 print('znaleziono zbędne prefixy w URLu: ' + link)
                 continue
-            # pomijaj link jeśli NIE zawiera jednego z tych słów:
-            prefixy_2 = ('contact', 'Contact', 'contacts', 'Contacs', 'kontakt', 'Kontakt', 'about-us', 'About-us', 'About-Us', 'aboutus', 'Aboutus', 'about', 'About',
-                'findus', 'find-us', 'Findus', 'Find-us', 'Find-Us', 'contact-us', 'Contact-Us', 'Contact-us', 'contactus', 'Contactus', 'meetus',
-                'meet-us')
+            # skip link if HAS NOT one of the following words:
+            prefixy_2 = ('contact', 'Contact', 'contacts', 'Contacs', 'kontakt', 'Kontakt', 'about-us', 'About-us', 'About-Us', 'aboutus', 'Aboutus', 'about', 'About', 'findus', 'find-us', 'Findus', 'Find-us', 'Find-Us', 'contact-us', 'Contact-Us','Contact-us', 'contactus', 'Contactus', 'meetus','meet-us')
             if not any(s in link for s in prefixy_2):
                 continue
 
@@ -111,10 +106,10 @@ while len(csv_urls):
             if not link in new_urls and not link in processed_urls:
                 new_urls.append(link)
 
-# sprawdza i usuwa zduplikowane rekordy w csv na sam koniec procesu
+# check and remove duplicated URLs in CSV result file
 seen = set() # set for fast O(1) amortized lookup
 for line in fileinput.FileInput('results.csv', inplace=1):
     if line not in seen:
         seen.add(line)
-        print (line.strip('\n')), # standard output is now redirected to the file + usuwa '\n' na końcu maili
+        print (line.strip('\n')), # standard output is now redirected to the file
 print(seen)
